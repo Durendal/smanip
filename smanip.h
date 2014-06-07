e8e37cc2d94106b77e59ca0e7d8612b7c0ca8740
Brian Hewitt <sir.durendal@gmail.com>
2014-06-07 13:25:44 -0600

    smanip.h
    Header file for smanip, contains prototypes for the library functions and defines constant value "NOT_PRES" used in the strpos() function to denote a failed search.

---
+++ smanip.h
@@ -0,0 +1,14 @@
+#ifndef _smanip_h
+#define _smanip_h
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define NOT_PRES -1
+
+ char *strtolower(char *str, int strleng);
+ char *strtoupper(char *str, int strleng);
+ int strpos(char *haystack, char *needle);
+ char *substr(char *str, int start, int len, int strleng);
+
+#endif
