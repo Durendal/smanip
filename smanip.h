61facd2b9327da24683d7451bac278f3f44f0149
Brian Hewitt <sir.durendal@gmail.com>
2014-06-09 14:20:24 -0600

    Update smanip.h
    added str_replace function prototype

--- smanip.h
+++ smanip.h
@@ -10,5 +10,6 @@
  char *strtoupper(char *str, int strleng);
  int strpos(char *haystack, char *needle);
  char *substr(char *str, int start, int len, int strleng);
+ char *str_replace (const char *, const char *, const char *);
 
 #endif
